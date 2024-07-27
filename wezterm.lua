03a119d9ae39283e7a5fc21c582466983f638e39
Myles Mo <mylesmo.ash@gmail.com>
2024-07-27 10:55:25 +0800

    fix: add launch config

--- wezterm.lua
+++ wezterm.lua
@@ -6,4 +6,5 @@ require("event.status").setup()
 return Config:init()
   :append(require("config.ui"))
   :append(require("config.keymap"))
+  :append(require("config.launch"))
   :append(require("config.theme")).options
